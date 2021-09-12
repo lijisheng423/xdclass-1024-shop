39594cb1aec301c48e85792241564817f9961f66
lijisheng <lijisheng1994@163.com>
2021-09-12 22:45:56 +0800

    Maven聚合工程创建微服务项目-项目添加公共包-测试lombok插件

--- xdclass-user-service/src/main/java/User.java
+++ xdclass-user-service/src/main/java/User.java
@@ -1,10 +1,14 @@
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
-@Getter
-@Setter
+@Data
+@Slf4j
 public class User {
     private int age;
     private String userName;
     private String idCard;
+
+    public void test(){
+        log.info("测试日志输出");
+    }
 }
