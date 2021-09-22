0617baca1a27e351ad7497c201ce0af13856d28d
lijisheng <lijisheng1994@163.com>
2021-09-22 22:32:22 +0800

    用户微服务开发-邮件发送介绍和项目整合spring邮箱starter

---
+++ xdclass-user-service/src/main/java/net/xdclass/service/MailService.java
@@ -0,0 +1,6 @@
+package net.xdclass.service;
+
+public interface MailService {
+
+    void sendMail(String to,String subject,String content);
+}
---
+++ xdclass-user-service/src/test/java/net/xdclass/biz/MailTest.java
@@ -0,0 +1,24 @@
+package net.xdclass.biz;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.UserApplication;
+import net.xdclass.service.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserApplication.class)
+@Slf4j
+public class MailTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Test
+    public void testSendMail(){
+        mailService.sendMail("1324549476@qq.com","欢迎来到小D课堂","测试邮件的发送，http://www.baidu.com");
+    }
+}
