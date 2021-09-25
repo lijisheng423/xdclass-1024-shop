05f2f759be1b858cac588f3e07d9ef225e23d1ff
lijisheng <lijisheng1994@163.com>
2021-09-26 00:35:47 +0800

    用户微服务开发-用户微服务头像上传阿里云OSS接口和SwaggerUI提效

--- xdclass-common/src/main/java/net/xdclass/enums/BizCodeEnum.java
+++ xdclass-common/src/main/java/net/xdclass/enums/BizCodeEnum.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 /**
  * 小滴课堂,愿景：让技术不再难学
+ *
  * @Description 状态码定义约束，共6位数，前三位代表服务，后4位代表接口
  * 比如 商品服务210,购物车是220、用户服务230，403代表权限
  */
@@ -11,22 +12,27 @@ public enum BizCodeEnum {
     /**
      * 通用操作码
      */
-    OPS_REPEAT(110001,"重复操作"),
+    OPS_REPEAT(110001, "重复操作"),
 
     /**
-     *验证码
+     * 验证码
      */
-    CODE_TO_ERROR(240001,"接收号码不合规"),
-    CODE_LIMITED(240002,"验证码发送过快"),
-    CODE_ERROR(240003,"验证码错误"),
-    CODE_CAPTCHA_ERROR(240101,"图形验证码错误"),
+    CODE_TO_ERROR(240001, "接收号码不合规"),
+    CODE_LIMITED(240002, "验证码发送过快"),
+    CODE_ERROR(240003, "验证码错误"),
+    CODE_CAPTCHA_ERROR(240101, "图形验证码错误"),
 
     /**
      * 账号
      */
-    ACCOUNT_REPEAT(250001,"账号已经存在"),
-    ACCOUNT_UNREGISTER(250002,"账号不存在"),
-    ACCOUNT_PWD_ERROR(250003,"账号或者密码错误");
+    ACCOUNT_REPEAT(250001, "账号已经存在"),
+    ACCOUNT_UNREGISTER(250002, "账号不存在"),
+    ACCOUNT_PWD_ERROR(250003, "账号或者密码错误"),
+
+    /**
+     * 文件上传
+     */
+    FILE_UPLOAD_USER_IMG_FILE(600101, "用户头像文件上传失败");
 
     @Getter
     private String message;
@@ -34,7 +40,7 @@ public enum BizCodeEnum {
     @Getter
     private int code;
 
-    private BizCodeEnum(int code,String message){
+    private BizCodeEnum(int code, String message) {
         this.code = code;
         this.message = message;
     }
--- xdclass-user-service/src/main/java/net/xdclass/controller/UserController.java
+++ xdclass-user-service/src/main/java/net/xdclass/controller/UserController.java
@@ -2,13 +2,22 @@ package net.xdclass.controller;
 
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import net.xdclass.enums.BizCodeEnum;
+import net.xdclass.service.FileService;
+import net.xdclass.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 二当家小D
@@ -18,6 +27,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user/v1")
 public class UserController {
+
+    @Autowired
+    private FileService fileService;
+
+    @ApiOperation("用户头像上传")
+    @PostMapping(value = "upload")
+    public JsonData uploadUserImg(
+            @ApiParam(value = "文件上传", required = true)
+            @RequestPart("file") MultipartFile file) {
+
+        String result = fileService.uploadUserImg(file);
+
+        return result != null ? JsonData.buildSuccess(result) : JsonData.buildResult(BizCodeEnum.FILE_UPLOAD_USER_IMG_FILE);
+
+    }
 
 }
 
