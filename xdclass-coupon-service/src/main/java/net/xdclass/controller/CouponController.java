0efc6eae696dbd988d4340907d39a9ce4cda8341
lijisheng <lijisheng1994@163.com>
2022-06-26 11:30:56 +0800

    优惠券微服务-项目整合redisson

--- xdclass-coupon-service/src/main/java/net/xdclass/controller/CouponController.java
+++ xdclass-coupon-service/src/main/java/net/xdclass/controller/CouponController.java
@@ -4,13 +4,17 @@ package net.xdclass.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import net.xdclass.enums.CouponCategoryEnum;
 import net.xdclass.service.CouponService;
 import net.xdclass.util.JsonData;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -22,11 +26,15 @@ import java.util.Map;
  */
 @Api(tags = "优惠券模块")
 @RestController
+@Slf4j
 @RequestMapping("/api/coupon/v1")
 public class CouponController {
 
     @Autowired
     private CouponService couponService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @ApiOperation("分页查询优惠券")
     @GetMapping("page_coupon")
@@ -44,6 +52,26 @@ public class CouponController {
         JsonData jsonData = couponService.addCoupon(couponId, CouponCategoryEnum.PROMOTION);
         return JsonData.buildSuccess();
     }
+
+
+/*    @GetMapping("lock")
+    public JsonData testLock(){
+        RLock lock = redissonClient.getLock("lock:coupon:1");
+        //阻塞等待
+        //lock.lock(10, TimeUnit.MILLISECONDS);
+        lock.lock();
+
+        try {
+            log.info("加锁成功，处理业务逻辑。。。。。"+Thread.currentThread().getId());
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log.info("解锁成功，其他现成可以进入。。。。"+Thread.currentThread().getId());
+            lock.unlock();
+        }
+        return JsonData.buildSuccess();
+    }*/
 
 }
 
