cf1546be7a7f4d9669b60b7aee405f64ea37ad65
lijisheng <lijisheng1994@163.com>
2022-06-26 18:15:18 +0800

    商品微服务-MybatisPlusGenerator代码自动生成工具

---
+++ xdclass-product-service/src/main/java/net/xdclass/ProductApplication.java
@@ -0,0 +1,15 @@
+package net.xdclass;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("net.xdclass.mapper")
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
