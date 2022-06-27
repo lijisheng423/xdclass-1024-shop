7f8761b3f9490481c25e7d90ae4ecd9d4adbca7b
lijisheng <lijisheng1994@163.com>
2022-06-27 22:01:13 +0800

    订单微服务项目-拦截器-枚举类配置

---
+++ xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderItemServiceImpl.java
@@ -0,0 +1,10 @@
+package net.xdclass.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.service.ProductOrderItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ProductOrderItemServiceImpl implements ProductOrderItemService {
+}
