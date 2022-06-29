e2eb84eee2cb78b32bf2968605fc8fec670f7b8b
lijisheng <lijisheng1994@163.com>
2022-06-29 22:52:49 +0800

    多个微服务服务引入Nacos注册中心和Feign依赖

--- xdclass-user-service/src/main/java/net/xdclass/UserApplication.java
+++ xdclass-user-service/src/main/java/net/xdclass/UserApplication.java
@@ -3,8 +3,12 @@ package net.xdclass;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 @MapperScan("net.xdclass.mapper")
 public class UserApplication {
     public static void main(String[] args) {
