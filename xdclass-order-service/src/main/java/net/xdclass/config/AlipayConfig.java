909cab6c49ce2532239375ddd5d3f6749dbc203f
lijisheng <lijisheng1994@163.com>
2022-07-16 16:17:31 +0800

    订单微服务-支付配置

---
+++ xdclass-order-service/src/main/java/net/xdclass/config/AlipayConfig.java
@@ -0,0 +1,35 @@
+package net.xdclass.config;
+
+public class AlipayConfig {
+
+    /**
+     * 支付宝 APPID
+     */
+    public static final String APPID = "2021000121626791";
+
+    /**
+     * 应用私钥
+     */
+    public static final String APP_PRI_KEY = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCJzpi1F9cgUvyLndKogdeR1oj64ItxAEbZTaZUBFCMXj7hIUaa2Tg4f8bCRY0u8kaivD9esrZivlHt4wxmiy7GtI5lox9ng/GybkHNFOxL7rpMfLrzDslUrIYN9tl4XYb4Q3M1Y1tPtJquVPy6wwKZ2C1xjw337PPQOx4rSgvHh8m8oVthwIVNwUm3M/ba6S47Ga8iw3VAKhMayK79co/J7m/xvr0AmRgQ/GydIOnKx25s2XTByotFOSTW+hi3tEnNR+fYOyDcQ6KpBW53jgaXkjQvi9JomNEGhpwrNuFWrK5u4O3mL8I346Z/kfHDoiKf3NjSQ+WJSXDOfpl5b/3NAgMBAAECggEARkI7nxCRkJxev7vuPBU+187eNbInxawTcxTguTMd8gLW4ib5/VbEECItLr0RFx9UngGF+Xz5+ev4j2VT3DAX4B0MAIujJj6iCoumJq4FL01+VCK/mc4vHWl8S0BQbcqZVpfk9BI86q00s68Gzx0/hTovKRd7UqzPPZn2brGowcgcjHbW/oWWINZMF1Knkq7j65oC//eoqqXE9IqZkvzTP9QBP6zsmf2TOGVjXlA89jahtCLsWbD18e2O9Ts1KVURKlxnW6OyJ8XgLwAdBvPX+80dQ9uSyiUUyeqiMOGd/dO1XRBBuQmo9NwxlYEjz9/sNRxSaoThcn5VT4aLQ2YiIQKBgQDDEcSwG/g6vunmuGnA4vZOQZYSkM3P4cqlOt7n/7s5VNnZ7YAaogLTSCcfpTqYvsBqjzwEra1NyftxmS6WuVbxoz27tnsTYz4szGnvODxRtU4xZwkANvdbY3N4lK/QSR8xjWlOjMO9WDzSaASNKXDHIGKasJv7UEr5gDuDNG7VaQKBgQC02fxuTkZWG8pJJ8JGK6GEulQkkLabwDl56pzA40usgigvG3NaKpfqEgFdZbXDKhRknogQzP78qB6McbeJ1cVwn+z/s4096/JIlQWgZiJfhxdAMxYauq8ZC6dQtCglnQ70HRniIk0DIWU9s/m4G2DpstpNCOAwtwfMqmkJdaskxQKBgQCrb6k50UsGbhTo9XfT0mP/gLqg1Gxc36aWMjgHEwR7LWxMj+M/BNWQ1F+1+yOVbEd1Hy7zOaQOso3uV17PjlGwCfROTeC6uETDYF9Vo0m30azH1DLf4m7f/XZVKe+eETV9+jbrluDMPoktrccaPKpmBrtCEibz5qwNvLoQ5HrnyQKBgByBh5JzcA6+jqH9TFf3JJxhKkYhMUdXgze+TTryGMl2MpNKRmJd+FgCwohQMdgzV8eMXXIYfdChRz9n5UAqm9ZmvtqHGKo7+JKMOrjPoHdiOb36z28jIdpWxeaonvNe0C6WL7A/K9hkeSrCpYfpeNsdP9+ImJ3VG7uc2LWPAIzpAoGAfI6OYJLF3J8ZVc8aZMf9CSEx/zV0gBu7vG8DQeMoFqolV7WXND92L2sqq7fXYlpuUSyMQ7/Vw0yt138qMdUU6V7rqycd2HyWGnRtah4QfNXFGr9ycngGl5wF3pFSjlIZ55YJV/C7d9MNC9wu2ylR6RpzvpiM8T3g6bJyoUIyM6w=";
+
+    /**
+     * 支付宝公钥
+     */
+    public static final String ALIPAY_PUB_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAq3s8ihJ958WNbEz7ACnlyVgzzR2a1K/y/Gzus8fIUe2OL+q7nQb42u2aR4vGSiH/LbBrWCnpWfSjKVkk1++GPQC26RnmGUt++m1BofhJTUKmgVhNwE5pD6j/Z+l3Ut/uEjDeWLElCPOcv+Y7eS9o1lrmL035vZvXmcPp7U6O55/0g/UhzqVBNbCRG4E470rWjWLWfFOiUKTTwlgSD/BkR1kFScyt0J+KpLy7Bki0z9VnQl9Cb7Bj3O0JuTsssC+uiN/6TEVIIyhK0jEaPXYJ1ipyMP68lilNd1hpmxWf13E1S1WsfIFpQuBKAkM9bH6xaAfr6ocAQnv5OYpxFQ2OtwIDAQAB";
+
+    /**
+     * 签名类型
+     */
+    public static final String SIGN_TYPE = "RSA2";
+
+    /**
+     * 字符编码
+     */
+    public static final String CHARSET = "UTF-8";
+
+    private AlipayConfig(){
+
+    }
+
+
+}
