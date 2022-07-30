9da0b4dc38ef8e4b565e823ff788f03f8bc866b3
lijisheng <lijisheng1994@163.com>
2022-07-30 17:55:10 +0800

    订单微服务下单支付全链路多场景测试准备工作

--- xdclass-common/src/main/java/net/xdclass/constant/TimeConstant.java
+++ xdclass-common/src/main/java/net/xdclass/constant/TimeConstant.java
@@ -6,5 +6,7 @@ public class TimeConstant {
      * 支付订单的有效时长，超过未支付则订单关闭
      * 订单超时，毫秒，默认30分钟
      */
-    public static final long ORDER_PAY_TIMEOUT_MILLS = 30*60*1000;
+    //public static final long ORDER_PAY_TIMEOUT_MILLS = 30*60*1000;
+    public static final long ORDER_PAY_TIMEOUT_MILLS = 5*60*1000;
+
 }
